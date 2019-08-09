a8e569207946a9aa1dea71db213a780c4196b602
David Hay <hay.david@hotmail.com>
2019-08-09 21:38:53 +0000

    node server 2

--- my-app.js
+++ my-app.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const https = require('https');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = https.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
@@ -11,4 +11,22 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const url = "https://jsonplaceholder.typicode.com/posts/1";
+const exercises = []
+
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+
+    console.log(body);
+    exercises.push(body)
+    console.log(exercises)
+  });
 });
